0e9c5f0632eb37d3fc47e5dac0ab3d6682d613cb
tovar <tovar@duck.com>
2023-08-06 11:36:17 -0400

    updated geo file

--- js/geo.js
+++ js/geo.js
@@ -1,32 +1,39 @@
 // Geolocation
-// window.addEventListener('load', () => {
-//     navigator.geolocation.getCurrentPosition(handleLoad);
-//     navigator.geolocation.watchCurrentPosition(handlePositionUpdate, handleError, options);
-//    })
 
-// window.addEventListener('unload', () => {
-//     console.log('Location no longer tracked...')
-//     navigator.geolocation.clearWatch(watchID)
-// })
-const options = {
-  maximumAge: 10000,
-  timeout: 5000,
-  enableHighAccuracy:true
-}
+if('geolocation' in navigator) {
+  const options = {
+    maximumAge: 10000,
+    timeout: 5000,
+    enableHighAccuracy:true
+  }
+  
+  const geo = navigator.geolocation;
+  
+  geo.getCurrentPosition(handleGetPosition, handleError, options);
+  geo.watchPosition(handlePositionUpdate, handleError, options);
+  
+  function handleGetPosition(position) {
+    const { latitude, longitude } = position.coords;
+    console.log(`Lat: ${latitude}, Lon: ${longitude}`);
+  }
+  
+  function handlePositionUpdate(position) {
+    const { latitude, longitude } = position.coords;
+    console.log(`Update! Lat: ${latitude}, Lon: ${longitude}`);
+  }
+  
+  function handleError(error) {
+   const {code, message} = error
+    if (code === 2) {
+      console.log(`Error 2 but don't worry about it`)
+    } return
+  }
+  
+} else {
+  alert('Your browser does not have Geolocation capabilities. Sorry!')
+} return
 
-navigator.geolocation.getCurrentPosition(handleGetPosition, handleError, options);
-navigator.geolocation.watchPosition(handlePositionUpdate, handleError, options);
 
-function handleGetPosition(geo) {
-  const { latitude, longitude } = geo.coords;
-  console.log(`Lat: ${latitude}, Lon: ${longitude}`);
-}
 
-function handlePositionUpdate(position) {
-  console.log(position.coords);
-}
 
-function handleError(error) {
-  console.error(error);
-}
 
