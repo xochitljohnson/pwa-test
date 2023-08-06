28443d581e73df71ba7a3fac058f7c3c572b7437
tovar <tovar@duck.com>
2023-08-05 21:03:15 -0400

    messing around with geo

--- js/geo.js
+++ js/geo.js
@@ -8,23 +8,25 @@
 //     console.log('Location no longer tracked...')
 //     navigator.geolocation.clearWatch(watchID)
 // })
+const options = {
+  maximumAge: Infinity,
+  timeout: 500,
+  enableHighAccuracy:true
+}
 
-navigator.geolocation.getCurrentPosition(handleLoad);
-navigator.geolocation.watchPosition(handlePositionUpdate, handleError);
+navigator.geolocation.getCurrentPosition(handleLoad, handleError, options);
+navigator.geolocation.watchPosition(handlePositionUpdate, handleError, options);
 
-  function handleLoad(geo) {
-    const { latitude, longitude } = geo.coords;
-    console.log(`Lat: ${latitude}, Lon: ${longitude}`);
-  }
+function handleLoad(geo) {
+  const { latitude, longitude } = geo.coords;
+  console.log(`Lat: ${latitude}, Lon: ${longitude}`);
+}
 
+function handlePositionUpdate(position) {
+  console.log(position.coords);
+}
 
-  function handlePositionUpdate(position) {
-   
-    console.log(position.coords);
+function handleError(error) {
+  console.log(`Error code ${error.code}`);
+}
 
-  }
-
-  function handleError(error) {
-    console.error(error)
-  }
-  
